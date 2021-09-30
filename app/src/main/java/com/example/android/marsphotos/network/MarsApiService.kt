b137eafeba2c4b3ca666cb37df2df295e70a16a9
Fernando de Paula <dipaula018@gmail.com>
2021-09-29 22:11:50 -0300

    feat: finished

---
+++ app/src/main/java/com/example/android/marsphotos/network/MarsApiService.kt
@@ -0,0 +1,53 @@
+package com.example.android.marsphotos.network
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+
+private const val BASE_URL = "https://android-kotlin-fun-mars-server.appspot.com"
+
+/**
+ * Build the Moshi object with Kotlin adapter factory that Retrofit will be using.
+ */
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
+/**
+ * The Retrofit object with the Moshi converter.
+ */
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(MoshiConverterFactory.create(moshi)) //converte JSON
+    .baseUrl(BASE_URL)
+    .build()
+
+/**
+ * A public interface that exposes the [getPhotos] method
+ */
+interface MarsApiService {
+    /**
+     * Returns a [List] of [MarsPhoto] and this method can be called from a Coroutine.
+     * The @GET annotation indicates that the "photos" endpoint will be requested with the GET
+     * HTTP method
+     */
+    @GET("photos")
+    suspend fun getPhotos(): List<MarsPhoto>
+}
+
+/*
+* Observação: lembre-se de que a "instanciação lenta" ocorre quando a criação do objeto é atrasada
+*  até que seja necessária para evitar cálculos
+*  ou o uso desnecessário outros recursos de computação
+* */
+
+
+/**
+ * A public Api object that exposes the lazy-initialized Retrofit service
+ */
+object MarsApi {
+    val retrofitService: MarsApiService by lazy {
+        retrofit.create(MarsApiService::class.java)
+    }
+}
